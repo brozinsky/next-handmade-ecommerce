ef8a9b5e55201cfd7cb7456bfbe4415986154cbd
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2024-01-28 21:28:47 +0100

    chore(home): add linking to category tiles, remove unused hook

--- src/components/layout/Categories.tsx
+++ src/components/layout/Categories.tsx
@@ -6,7 +6,6 @@ import BasketImg from "@/public/koszyk-1.jpg";
 import OtherImg from "@/public/inne-1.webp";
 import clsx from "clsx";
 import ScrollAnimated from "../utils/ScrollAnimated";
-import useMenuTabsStore from "../../stores/useMenuTabsStore";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
 import CategoryTile from "../ui/CategoryTile";
 import HeadingLine from "../ui/HeadingLine";
@@ -17,30 +16,17 @@ type Props = {
 };
 
 export default function Categories({ pb = "md", pt = "md" }: Props) {
-  const { setTab } = useMenuTabsStore();
   const isMobile = useMediaQuery(`(max-width: 1280px)`);
 
   return (
     <section id="Categories" className={"pt-section-half"}>
       <ScrollAnimated className={clsx(!isMobile && "container container--xs")}>
         <HeadingLine textPosition="center">Kategorie</HeadingLine>
-
         <div className="grid grid-cols-2 gap-2 sm:gap-6 md:gap-2 lg:gap-8">
-          <CategoryTile tabId={1} title={"Opaski"} imgSrc={HeadbandImg} />
-          <CategoryTile tabId={3} title={"Plecaki i torebki"} imgSrc={BagImg} />
-          <CategoryTile tabId={2} title={"Koszyki"} imgSrc={BasketImg} />
-          <CategoryTile tabId={4} title={"Inne"} imgSrc={OtherImg} />
-
-
-          {/* <CategoryTilePrimary tabId={2} title={"Koszyki"} imgSrc={Image1} />
-          <CategoryTilePrimary tabId={3} title={"Plecaki"} imgSrc={Image1} />
-          <CategoryTilePrimary tabId={4} title={"Torebki"} imgSrc={Image1} />
-          <CategoryTilePrimary
-            tabId={5}
-            title={"Dostępne od ręki"}
-            imgSrc={Image1}
-          />
-          <CategoryTilePrimary tabId={6} title={"Kursy"} imgSrc={Image1} /> */}
+          <CategoryTile value="opaski" title={"Opaski"} imgSrc={HeadbandImg} />
+          <CategoryTile value="plecaki" title={"Plecaki i torebki"} imgSrc={BagImg} />
+          <CategoryTile value="koszyki" title={"Koszyki"} imgSrc={BasketImg} />
+          <CategoryTile value="inne" title={"Inne"} imgSrc={OtherImg} />
         </div>
       </ScrollAnimated>
     </section>
--- src/components/ui/CategoryTile.tsx
+++ src/components/ui/CategoryTile.tsx
@@ -1,33 +1,26 @@
 "use client";
 import React from "react";
-import { motion } from "framer-motion";
 import Image, { StaticImageData } from "next/image";
-import clsx from "clsx";
 import Link from "next/link";
-import useMenuTabsStore from "../../stores/useMenuTabsStore";
-import Button from "./Button";
-import { X, Loader, ArrowRight, ArrowUpRight } from "react-feather";
+import { ArrowUpRight } from "react-feather";
 
 type Props = {
   imgSrc: StaticImageData;
   title: string;
+  value: string;
   width?: number;
   height?: number;
-  span?: number;
-  tabId: number;
 };
 
 export default function CategoryTile({
   imgSrc,
   title,
+  value,
   width = 384,
   height = 256,
-  span = 1,
-  tabId,
 }: Props) {
-  const { setTab } = useMenuTabsStore();
   return (
-    <div className="relative overflow-hidden cursor-pointer group">
+    <Link href={`/sklep/${value}`} className="relative overflow-hidden cursor-pointer group">
       <div className="aspect-4/3">
         <Image
           className="object-cover w-full h-full transition duration-500 rounded-lg group-hover:scale-110"
@@ -41,19 +34,13 @@ export default function CategoryTile({
       <div className="absolute bottom-0 left-0 right-0 w-full h-full transition duration-500 bg-black bg-opacity-10 rounded-2xl group-hover:bg-opacity-0">
         <div className="absolute w-7/12 px-4 py-6 transform -translate-x-1/2 rounded-lg bottom-4 bg-primary-300/80 left-1/2">
           <h3 className="text-xl font-medium text-center uppercase text-primary-900">
-            <span className="flex items-center justify-center gap-2">
+            <span className="flex items-center justify-center gap-2 select-none">
               {title}
               <ArrowUpRight />
             </span>
           </h3>
-          {/* <button className="w-full px-6 py-3 text-xs font-medium text-white border rounded-full lg:text-md btn-zoom">
-            Przejdź
-          </button> */}
-          {/* <Button onClick={() => console.log('klik')} variant="primary" shape="rectangle">
-            <span className="flex items-center gap-2">Przejdź <ArrowUpRight className="h-4"/></span>
-          </Button> */}
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
