7d24ae064f2125767f85b43e6ae979bee943c40e
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2024-01-28 18:38:25 +0100

    chore(404): add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,35 @@
+import PageWrapper from "@/components/utils/PageWrapper";
+import NotFoundSVG from "@/public/404.svg";
+import Image from "next/image";
+import Button from "@/components/ui/Button";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <PageWrapper className="overflow-x-hidden">
+      <section className="py-section h-screen-footer">
+        <div className="container justify-center container--sm flex-items-center">
+          <Image
+            className="w-full h-full max-w-sm mx-auto opacity-80"
+            src={NotFoundSVG}
+            priority
+            alt="404 nie znaleziono"
+          />
+          <div className="mx-auto">
+            <h1 className="mt-10 mb-4 text-3xl font-medium text-center">
+              Ups! Nie znaleziono strony.
+            </h1>
+            <p className="mb-6 text-lg text-center font-regular">
+              Niestety, nie możemy znaleźć strony, której szukasz.
+            </p>
+            <Link className="block mx-auto w-fit" href="/">
+              <Button variant="primary" shape="rectangle">
+                Wróć do strony głównej
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </PageWrapper>
+  );
+}
