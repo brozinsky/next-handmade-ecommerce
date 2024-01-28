8e5deff9208459b0cd885ecd6295f8d330ad75ab
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2024-01-28 21:25:37 +0100

    chore(contact): add min height to wrapper on contact page

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -4,7 +4,9 @@ import PageWrapper from "@/components/utils/PageWrapper";
 export default function ContactPage() {
   return (
     <PageWrapper className="bg-light-ivory">
-      <Contact />
+      <div className="min-h-screen-footer flex-center-center">
+        <Contact />
+      </div>
     </PageWrapper>
   );
 }
